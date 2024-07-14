f69ffb8ad8cad5b0758ece9c4a8717c6eeb0a016
tornike <tornikeeloshvili77@gmail.com>
2024-07-14 13:12:07 +0400

    add: routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,25 @@
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import Router from "@/routes";
+
 function App() {
-  return <></>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem">
+            <Router />
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/routes.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<div> Hello Nigger </div>}/>
+      <Route path='/2' element={<div> Hello Nigger 2 </div>}/>
+    </Routes>
+  )
+}
+
+export default Router
